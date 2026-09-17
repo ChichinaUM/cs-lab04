8160763d20b371fb95010e246353afb3681e24d0
Bob (ChichinaUM) <lim550@mail.ru>
2026-09-17 21:38:55 +0300

    build: А теперь внес правки я, Боб

--- main.java
+++ main.java
@@ -5,6 +5,8 @@ public class main
 		/////////////////////////
 		///  А + В
 		
+		/// Правки, внесенные Бобом - AA * BB
+		
 	    Mobile_device md = new Mobile_device("Windows 11", "Aple", "11 inch", "Gray", 11400);
 	    System.out.println("Значение полей после инициализации объекта: \n"); 
 	    md.printInform();
